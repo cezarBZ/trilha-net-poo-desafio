0c078341bb4b42ff7d227b5dcf94433b00be5969
Cezar dos Santos Bezerra <cezar.bezerra@programmers.com.br>
2023-03-26 20:37:17 -0300

    Challenge solved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Iphone iphone = new Iphone("8199998888", "Iphone 14", "1234", 256);
+Nokia nokia = new Nokia("8166663333", "Nokia tijolão", "5678", 128);
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+iphone.Ligar();
+nokia.Ligar();
+
+iphone.ReceberLigacao();
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Instagram");
